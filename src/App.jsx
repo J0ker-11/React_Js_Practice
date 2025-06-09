763c82b6e1c3cd8e5f3f8175b89ee393abb82142
joker-11 <primetech444@gmail.com>
2025-06-09 15:12:43 +0100

    More component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import Header from './Header.jsx'
+import Footer from './Footer.jsx'
+import Food from './Food.jsx'
+
+
+
+
+
 function App() {
   return(
     <>
       <Header/>
+      <Food/>
+      <Footer/>
     </>
   )
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+
+function Footer() {
+
+    return(
+        <footer>
+            <p> {new Date().getFullYear()} &copy; PRIME TECHNOLOGIES LIMITED</p>
+        </footer>
+    );
+}
+
+export default Footer
