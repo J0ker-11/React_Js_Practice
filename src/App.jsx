a76c66b5b4ed3cd3afe60d907f31650b7239985e
joker-11 <primetech444@gmail.com>
2025-06-09 15:57:59 +0100

    Card component created

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import Food from './Food.jsx'
-
+import Card from './Card.jsx'
 
 
 
@@ -11,6 +11,10 @@ function App() {
   return(
     <>
       <Header/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
       <Food/>
       <Footer/>
     </>
