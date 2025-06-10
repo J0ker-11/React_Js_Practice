aed865b02b4518a526776ac31bd598a2b2141637
joker-11 <primetech444@gmail.com>
2025-06-10 23:06:05 +0100

    Task Conditional Rendering Task 2

--- src/GreetingCardParent.jsx
+++ src/GreetingCardParent.jsx
@@ -6,8 +6,8 @@ function GreetingCardParent() {
     return(
         <>
             <GreetingCardChild username="Alice" isVip={true} isLoggedIn={true} />
-            <GreetingCardChild username="Bob" isVip={true} isLoggedIn={true} />
-            <GreetingCardChild username="" isVip={false} isLoggedIn={true} />
+            <GreetingCardChild username="Bob" isVip={false} isLoggedIn={true} />
+            <GreetingCardChild username="" isVip={false} isLoggedIn={false} />
         </>
     );
 }
