6c87e6dfbb6d891c5327f59581f15b6b3ae8020c
joker-11 <primetech444@gmail.com>
2025-06-11 07:15:08 +0100

    Task Conditional Rendering Task 2

--- src/GreetingCardParent.jsx
+++ src/GreetingCardParent.jsx
@@ -4,11 +4,29 @@ import GreetingCardChild from "./GreetingCardChild.jsx";
 function GreetingCardParent() {
 
     return(
-        <>
-            <GreetingCardChild username="Alice" isVip={true} isLoggedIn={true} />
-            <GreetingCardChild username="Bob" isVip={false} isLoggedIn={true} />
-            <GreetingCardChild username="" isVip={false} isLoggedIn={false} />
-        </>
+       <div className="bg-gray-200 m-h-screen p-8">
+        <h1 className="text-3xl font-bold text-center mb-8 text-gray-900">
+            Greeting Card Examples
+        </h1>
+
+        <div className="flex flex-wrap justify-center">
+            
+                <GreetingCardChild 
+                    isLoggedIn={true} 
+                    username="Alice" 
+                    isVip={true}/>
+
+                <GreetingCardChild 
+                    isLoggedIn={true} 
+                    username="Bob" 
+                    isVip={false}/>
+
+                <GreetingCardChild 
+                    isLoggedIn={false} 
+                    username="" 
+                    isVip={false}/>
+        </div>
+       </div>
     );
 }
 
--- src/UserGreetingsParent.jsx
+++ src/UserGreetingsParent.jsx
@@ -3,7 +3,17 @@ import UserGreetingsChild from "./UserGreetingsChild";
 function UserGreetingsParent() {
 
     return(
-        <UserGreetingsChild isLoggedIn={true} username={"Hassan"}/>
+        <div className="bg-red-200 m-h-screen p-8">
+            <h1 className="text-3xl font-bold text-center mb-8 text-gray-900">
+                Practice Greeting Card
+            </h1>
+            <div className="text-center">
+                <UserGreetingsChild 
+                    isLoggedIn={false} 
+                    username={"Hassan"}
+                />
+            </div>
+        </div>
     );
 }
 
